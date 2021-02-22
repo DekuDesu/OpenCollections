54bcd9b1c4aa376bcaa4ade1ac1a9454f3ea9416
Deku <39616108+DekuDesu@users.noreply.github.com>
2021-02-22 16:59:46 -0500

    Increased Reliability for StopObservingAsyncWorks()

--- OpenCollections.Test/ExtensionsTests.cs
+++ OpenCollections.Test/ExtensionsTests.cs
@@ -113,7 +113,7 @@ namespace OpenCollections.Tests
 
             consumer.Started += () => { consumerStarted = true; };
 
-            producer.Produce();
+            Task.WaitAll(producer.ProduceAsync());
 
             try
             {
