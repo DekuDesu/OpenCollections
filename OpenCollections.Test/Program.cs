54dd7f202ab30e7aaaf91262988d3cca65a9093c
Deku <39616108+DekuDesu@users.noreply.github.com>
2021-02-27 09:51:01 -0500

    Setup Basic Speed Testing

--- OpenCollections.Test/Program.cs
+++ OpenCollections.Test/Program.cs
@@ -1,3 +1,34 @@
 ﻿using System;
+using OpenCollections;
 
-Console.WriteLine("Hello World!");
+var producer = Factory.CreateProducer(Factory.CreateReader("Primes.txt"));
+
+var consumer = Factory.CreateConsumer<string, int>(producer);
+
+int itemsConsumed = 0;
+
+consumer.Operation = x =>
+{
+    System.Threading.Interlocked.Increment(ref itemsConsumed);
+    Console.WriteLine($"Items Consumed: {itemsConsumed}");
+    return int.Parse(x);
+};
+
+producer.Started += () => Log("Producer Started");
+producer.CollectionChanged += () => Log("Producer Collection Changed");
+producer.Finished += () => Log("Producer Finished");
+
+consumer.Started += () => Log("Consumer Started");
+consumer.CollectionChanged += () => Log("Consumer Collection Changed");
+consumer.Finished += () => Log("Consumer Finished");
+
+Console.WriteLine("Starting Producing");
+producer.ProduceAsync();
+Console.WriteLine("Ended Producing");
+
+Console.Read();
+
+void Log(string message)
+{
+    Console.WriteLine(message);
+}
